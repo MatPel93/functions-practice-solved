35454a13ab0eff56d3d502761b0dc6860dd0db5a
MatPel93 <matpel93@gmail.com>
2025-02-26 14:22:09 -0600

    completed all 5 js files exercises and confirmed they all pass run test

--- exercises/e5.js
+++ exercises/e5.js
@@ -1,8 +1,7 @@
-// In this exercise you'll complete a series of "comparison" mini exercises. 
-// Conditional logic is a huge part of coding. It's what helps a program make decisions. 
+// In this exercise you'll complete a series of "comparison" mini exercises.
+// Conditional logic is a huge part of coding. It's what helps a program make decisions.
 // Finish each exercise below.
 // You can use ANY type of function syntax you want: named, arrow, or expression with anonymous function.
-
 
 /** =========================
  * Create a getDiffTwentySeven(num) function to return the difference between a given number and 27
@@ -13,8 +12,13 @@
 
 // Your code goes here...
 
-
-
+const getDiffTwentySeven = (num) => {
+  if (num <= 27) {
+    return 27 - num;
+  } else {
+    return Math.abs(27 - num) * 2;
+  }
+};
 
 /** =========================
  * Create a sumOfTwoIntegers(num1, num2) function to compute and return the sum of the two given integers
@@ -25,8 +29,13 @@
 
 // Your code goes here
 
-
-
+const sumOfTwoIntegers = (num1, num2) => {
+  if (num1 != num2) {
+    return num1 + num2;
+  } else {
+    return (num1 + num2) * 3;
+  }
+};
 
 /** =========================
  * Create a isOneOfThemOrTheirSumIsFourty(num1, num2) function to check two given numbers and return a boolean.
@@ -39,19 +48,21 @@
 
 // Your code goes here...
 
-
-
+const isOneOfThemOrTheirSumIsFourty = (num1, num2) => {
+  if (num1 == 40 || num2 == 40) return true;
+  else if (num1 + num2 == 40) return true;
+  else return false;
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
   getDiffTwentySeven: getDiffTwentySeven || undefined,
   sumOfTwoIntegers: sumOfTwoIntegers || undefined,
   isOneOfThemOrTheirSumIsFourty: isOneOfThemOrTheirSumIsFourty || undefined,
-}
-export { f }
+};
+export { f };
